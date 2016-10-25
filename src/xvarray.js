df1e15e9f65981c1cd61b754433ceebfdc9bce07
W.S. Hager <wshager@gmail.com>
2016-10-25 17:42:10 +0200

    added more functions

--- src/xvarray.js
+++ src/xvarray.js
@@ -1,5 +1,7 @@
 import { List } from "immutable";
 
+import { error } from "xverr";
+
 import { seq, _first, _isSeq, _wrap, _wrapReduce, _partialRight } from "xvseq";
 
 const ListProto = List.prototype;
@@ -10,7 +12,7 @@ export function array(...a) {
 		return seq(List());
 	}
 	if(l==1 && _isSeq(a[0])){
-		return seq(List(a[0].toArray().map(_ => seq(_))));
+		return seq(List(a[0].flatten(true).toArray().map(_ => seq(_))));
 	}
 	return seq(List(a.map(_ => seq(_))));
 }
@@ -83,4 +85,29 @@ export function foldRight(...args){
 	var a = _first(args[0]);
 	if(!_isArray(a)) return error("XPTY0004");
 	return seq(a.reduceRight(_wrapReduce(fn),init));
+}
+
+function remove(...args){
+	if (args.length == 1) return error("XPTY0004");
+	var a = _first(args[0]);
+	var index = _first(args[1]) || 0;
+	if(!_isArray(a)) return error("XPTY0004");
+	if(!index) return seq(a);
+	return seq(a.delete(index.valueOf() - 1));
+}
+
+function append(...args){
+	if (args.length == 1) return error("XPTY0004");
+	var a = _first(args[0]);
+	var insert = args[1];
+	if(!_isArray(a)) return error("XPTY0004");
+	//console.log("insert", insert);
+	return seq(insert.isEmpty() ? a : a.push(insert));
+}
+
+function reverse(...args){
+	if (args.length === 0) return error("XPTY0004");
+	var a = _first(args[0]);
+	if(!_isArray(a)) return error("XPTY0004");
+	return seq(a.reverse());
 }
